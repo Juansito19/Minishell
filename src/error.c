e661a6bf619d74ca6c23a50ca14c53d38c55c9c4
Juan Brosio <juanbro68@gmail.com>
2025-12-01 22:58:11 +0100

    pwd.c - cd .c export.c unset.c export_utils.cft_array_dup.c -

--- src/error.c
+++ src/error.c
@@ -4,11 +4,23 @@
 /* ========= error ========== */
 /* ========================== */
 
-int ft_print_error(int error_code, char *s)
+int	ft_print_error(int error_code, char *s)
 {
 	if (s)
 		ft_fprintf(2, "%s\n", s);
 	if (error_code != 0)
 		perror(strerror(error_code));
+	return (1);
+}
+
+int	ft_pd_error(char *format, char *s)
+{
+	if (s)
+	{
+		ft_printf(format, s);
+		ft_printf("\n");
+	}
+	else
+		ft_printf(format);
 	return (1);
 }
